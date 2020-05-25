178347d45a89fea509b6b6568208d9da1aed62a8
ggy <1017240671@qq.com>
2020-05-25 18:21:19 +0800

    feat:

--- gos-portal/src/main/java/com/taimei/gos/portal/controller/HelloWorldController.java
+++ gos-portal/src/main/java/com/taimei/gos/portal/controller/HelloWorldController.java
@@ -1,19 +1,45 @@
 package com.taimei.gos.portal.controller;
 
+import com.taimei.gos.common.api.CommonResult;
+import com.taimei.gos.mapper.AdminMapper;
+import com.taimei.gos.model.Admin;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("helloWorld")
 @Api(tags = "HelloWorldController")
 public class HelloWorldController {
+    @Autowired
+    private AdminMapper adminMapper;
 
     @RequestMapping(value = "/sayHello", method = RequestMethod.GET)
     @ApiOperation("sayHello")
     public String sayHello() {
         return "helloWord";
+    }
+
+    @RequestMapping(value = "/addAdmin", method = RequestMethod.POST)
+    @ApiOperation("新建后台管理用户")
+    public CommonResult addAdmin(String userName, String password) {
+        Admin admin = new Admin();
+        admin.setUserName(userName);
+        admin.setPassword(password);
+        System.out.println(admin);
+        System.out.println("adminMapper" + adminMapper);
+        int count = adminMapper.insertSelective(admin);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @RequestMapping(value = "/test/{id}", method = RequestMethod.GET)
+    @ApiOperation("测试参数")
+    public CommonResult test(@PathVariable Integer id) {
+        return CommonResult.success(id);
     }
 }
