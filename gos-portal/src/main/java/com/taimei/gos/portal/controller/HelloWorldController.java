9cf52bb027d80718b46086ec4bc9bda5a5bff0dd
ggy <1017240671@qq.com>
2020-05-28 17:36:39 +0800

    feat: rocketMQ

--- gos-portal/src/main/java/com/taimei/gos/portal/controller/HelloWorldController.java
+++ gos-portal/src/main/java/com/taimei/gos/portal/controller/HelloWorldController.java
@@ -5,15 +5,28 @@ import com.taimei.gos.mapper.AdminMapper;
 import com.taimei.gos.model.Admin;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.listener.Topic;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("helloWorld")
 @Api(tags = "HelloWorldController")
 public class HelloWorldController {
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
     @Autowired
     private AdminMapper adminMapper;
+
+    @Value("${demo.rocketmq.topic}")
+    private String topic;
 
     @RequestMapping(value = "/sayHello", method = RequestMethod.GET)
     @ApiOperation("sayHello")
@@ -41,5 +54,15 @@ public class HelloWorldController {
     @ApiOperation("测试参数")
     public CommonResult test(@PathVariable Integer id) {
         return CommonResult.success(id);
+    }
+
+    @GetMapping("/syncNews")
+    public String syncNews(){
+        Message message = new Message();
+        message.setBody("同步消息".getBytes());
+        SendResult sendResult = rocketMQTemplate.syncSend(topic, message);
+        // 同步消息发送成功会有一个返回值，我们可以用这个返回值进行判断和获取一些信息
+        System.out.println(sendResult);
+        return "success";
     }
 }
--- gos-portal/src/test/java/com/taimei/gos/portal/GosMbgApplicationTests.java
+++ gos-portal/src/test/java/com/taimei/gos/portal/GosMbgApplicationTests.java
@@ -2,11 +2,15 @@ package com.taimei.gos.portal;
 
 import com.taimei.gos.mapper.AdminMapper;
 import com.taimei.gos.model.AdminExample;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,5 +25,4 @@ class GosMbgApplicationTests {
 		long l = adminMapper.countByExample(adminExample);
 		System.out.println(l);
 	}
-
 }
