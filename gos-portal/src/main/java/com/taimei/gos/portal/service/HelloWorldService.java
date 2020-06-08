714dd35047003dcf08e4571102b51bff3f449d45
ggy <1017240671@qq.com>
2020-06-08 16:16:24 +0800

    feat: rocketMQ

--- gos-portal/src/main/java/com/taimei/gos/portal/service/HelloWorldService.java
+++ gos-portal/src/main/java/com/taimei/gos/portal/service/HelloWorldService.java
@@ -1,16 +1,46 @@
 package com.taimei.gos.portal.service;
 
+import com.taimei.gos.mapper.UserMapper;
+import com.taimei.gos.mapper.UserWechatMapper;
+import com.taimei.gos.model.User;
+import com.taimei.gos.model.UserWechat;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RocketMQMessageListener(topic = "${demo.rocketmq.topic}", consumerGroup = "user_consumer")
 public class HelloWorldService implements RocketMQListener<String>{
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserWechatMapper userWechatMapper;
+
     @Override
     public void onMessage(String message) {
         System.out.printf("-------我正在消费了 StringConsumer received: %s \n", message);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED,
+            isolation = Isolation.DEFAULT, readOnly = false, timeout = -1)
+    public void testTransactional() {
+        User user = new User();
+        user.setMobile("13826172865");
+        user.setIsDelete((byte) 0);
+        user.setParentId(1);
+        int insert = userMapper.insert(user);
+        int i = 100/0;
+        UserWechat userWechat = new UserWechat();
+        userWechat.setUserId(insert);
+        userWechat.setAppId("asdfajslfas");
+        userWechatMapper.insert(userWechat);
+
     }
 
 }
--- gos-portal/src/test/java/com/taimei/gos/portal/service/HelloWorldServiceTest.java
+++ gos-portal/src/test/java/com/taimei/gos/portal/service/HelloWorldServiceTest.java
@@ -1,5 +1,68 @@
-import static org.junit.Assert.*;
+package com.taimei.gos.portal.service;
 
+import io.swagger.models.auth.In;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HelloWorldServiceTest {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    @Test
+    public void testTransactional() {
+//        helloWorldService.testTransactional();
+//        logger.trace("trace——————-");￿￿
+//        logger.debug("debug-------");
+//        logger.info("info-----");
+//        logger.warn("warn------");
+//        logger.error("error");
+//        int A[][] = { { 1, 2, 0 }, { 3, 4, 0 }, { 5, 6, 0 } };
+//        System.out.println(A[0]);
+        int a = 1001;
+        boolean b = this.IsPalindrome(a);
+        System.out.println(b);
+    }
+
+    public boolean IsPalindrome(int x) {
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        String s = String.valueOf(x);
+        char[] chars = s.toCharArray();
+        for(int i = 0 ; i < chars.length; i++ ) {
+            System.out.println(i);
+            if(i> chars.length/2 -1) {
+                return true;
+            }
+            int a = chars.length - (i+1);
+            System.out.println(i);
+            if(chars[i] !=  chars[a]) {
+                System.out.println("-------");
+                System.out.println(chars[i]);
+                System.out.println(chars[a]);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void test() {
+        System.out.println("merge dev");
+    }
 }
